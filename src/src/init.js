643376ddac6f2c276e9f18b90f71d5bd8c699364
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-06-10 20:36:04 +0800

    Add files via upload

---
+++ src/src/init.js
@@ -0,0 +1,20 @@
+const MEM = "GAMBLINGSHITLIMPEH"
+
+window.onload = ()=> {
+    localStorage.getItem(MEM) && Init()
+}
+
+function Init(){
+    document.getElementById("Notice").style.display = "none"
+    Display()
+}
+
+document.getElementById("Proceed").addEventListener("change", ()=> {
+    localStorage.setItem(MEM, true)
+    setTimeout(Init, 500)
+})
+
+document.addEventListener("keyup", e => {
+    if (e.key==" ")
+        localStorage.clear() || window.location.reload()
+})
